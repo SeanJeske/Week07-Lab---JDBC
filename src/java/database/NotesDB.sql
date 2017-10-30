0074a98d3f6ca8375bc270da24d018073f7ad065
SeanJeske <sean.jeske@edu.sait.ca>
2017-10-30 11:56:19 -0600

    JDBC Connection Created a notes section for a user with JDBC

--- src/java/database/NotesDB.sql
+++ src/java/database/NotesDB.sql
@@ -4,16 +4,13 @@ CREATE DATABASE NotesDB;
 USE NotesDB;
 
 
-DROP TABLE User;
+DROP TABLE Note;
 
-CREATE TABLE User( 
-    username VARCHAR(10) NOT NULL,
-    password VARCHAR(10) NOT NULL,
-    email VARCHAR(30) NOT NULL,
-    active BIT NOT NULL,
-    firstname VARCHAR(50) NOT NULL,
-    lastname VARCHAR(50) NOT NULL,
-    PRIMARY KEY (username)
+CREATE TABLE Note( 
+    noteid int NOT NULL AUTO_INCREMENT,
+    dateCreated DATE NOT NULL,
+    content VARCHAR(10000) CHARACTER SET utf8 NOT NULL,
+    PRIMARY KEY (noteid)
 );
 
-INSERT INTO User values('admin', 'password', 'test@test.com', 1, 'Bob', 'Bobberson');
+INSERT INTO Note (dateCreated,content) values(CURDATE(),'The quick brown fox jumped over the lazy dog');
